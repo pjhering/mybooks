1a6c32f4a1867d6ed0e2b8a2f477d46ec71aaf43
Pete Hering <pjhering@gmail.com>
2017-03-09 15:04:45 -0500

    Create PreviewPanelDialog.java

---
+++ src/test/java/mybooks/ui/PreviewPanelDialog.java
@@ -0,0 +1,23 @@
+package mybooks.ui;
+
+import javax.swing.JFrame;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class PreviewPanelDialog
+{
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("Preview PanelDialog");
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        frame.setSize(640, 480);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
+        
+        PanelDialog<AuthorPanel> dialog = new PanelDialog("New Author", new AuthorPanel());
+        AuthorPanel panel = dialog.open(frame);
+        if(panel != null)
+        {
+            frame.setContentPane(panel);
+        }
+    }
+}
